07dfaeba7d30f377fcd48ab9473a60c5f6b9ee90
Zeuxis Lo <seekstudio@gmail.com>
2015-09-04 14:38:31 +0800

    update backup db to dropbox script can work at ubuntu and debian

--- shell-backup-all-db-to-dropbox/backup.sh
+++ shell-backup-all-db-to-dropbox/backup.sh
@@ -19,7 +19,7 @@ databases=`$MYSQL_SELF -u $MYSQL_USER -p"$MYSQL_PASS" -e 'SHOW DATABASES;' | gre
 
 for database in $databases; do
     echo "Dumping $database"
-    $MYSQL_DUMP --force --opt -u $MYSQL_USER -p"$MYSQL_PASS" --database $database | gzip > "$BACKUP_DIR/$database.gz"
+    $MYSQL_DUMP --force --opt -u $MYSQL_USER -p"$MYSQL_PASS" --database $databases | gzip > "$BACKUP_DIR/$database.gz"
 done
 
 # Zip databases to one file then remove all databases
@@ -28,11 +28,17 @@ rm -rf $BACKUP_DIR
 
 # Put to dropbox
 put_cmd="/home/backup/dropbox_uploader.sh upload $BACKUP_DIR.tar.gz $DROPBOX_PATH/$TIMESTAMP.tar.gz"
-echo $put_cmd && `$put_cmd`
+echo $put_cmd && eval "$put_cmd"
 
 # Remove from dropbox (expire 6 day files)
-delete_cmd="/home/backup/dropbox_uploader.sh delete $DROPBOX_PATH/$(date -v -6d +'%F').tar.gz"
-echo $delete_cmd && `$delete_cmd`
+EXPIRE_DAY=$(date --date='6 days ago' +'%F')
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    EXPIRE_DAY=$(date -v -6d +'%F')
+fi
+
+delete_cmd="/home/backup/dropbox_uploader.sh delete $DROPBOX_PATH/$EXPIRE_DAY.tar.gz"
+echo $delete_cmd && eval "$delete_cmd"
 
 # Remove backup zip file
 rm -rf $BACKUP_DIR
